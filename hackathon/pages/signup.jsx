df54dd1ea7f920813b177c02bc8ecca0981b89d9
Shubham Kumar <141490044+sy730113@users.noreply.github.com>
2024-04-06 03:16:37 +0530

    Update signup.jsx

--- hackathon/pages/signup.jsx
+++ hackathon/pages/signup.jsx
@@ -14,7 +14,7 @@ export default function Signup() {
     e.preventDefault();
 
     try {
-      const res = await axios.post("http://localhost:5000/api/register", {
+      const res = await axios.post("https://well-food-api.vercel.app/register", {
         email,
         password,
         username,
